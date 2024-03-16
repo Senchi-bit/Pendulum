8b733e0ac062e851694d622435bbce9f49402ede
Арсен Ислямов <arsenislyam@gmail.com>
2024-03-17 00:05:03 +0300

    Add two timers for charts and pendulum

--- Pendulum/Pendulum/MainWindow.xaml.cs
+++ Pendulum/Pendulum/MainWindow.xaml.cs
@@ -28,8 +28,11 @@ namespace Pendulum
         private double Theta0 = 45;
         private double Alpha0 = 0;
         double[] xx = new double[2];
+        double[] xx2 = new double[2];
         double time = 0;
+        double time2 = 0;
         double dt = 0.03;
+        double dt2 = 0.03;
         Polyline pl = new Polyline();
         double xMin = 0;
         Double yMin = -100;
@@ -48,8 +51,11 @@ namespace Pendulum
         }
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            //Очистка всех графиков
             pointsX.Clear();
             pointsY.Clear();
+            energy.Clear();
+            times.Clear();
             Stop();
             double xMinFixed = 0; // Минимальное значение для оси X
             double xMaxFixed = 50; // Максимальное значение для оси X
@@ -71,86 +77,40 @@ namespace Pendulum
 
             model.Series.Add(series);
             plotView.Model = model;
+            //В случае неправильного ввода будут использованы начальные данные
             try
             {
                 PendulumMass = Convert.ToDouble(tbMass.Text);
                 PendulumLength = Convert.ToDouble(tbLength.Text);
                 DampingCoefficient = Convert.ToDouble(tbDamping.Text);
                 Theta0 = Convert.ToDouble(tbTheta0.Text);
-                dt = Convert.ToDouble(tbDt.Text);
+                dt2 = Convert.ToDouble(tbDt.Text);
             }
             catch {
                 PendulumMass = 1;
                 PendulumLength = 1; DampingCoefficient = 0.1;
                 Theta0 = 45;
-                Alpha0 = 0;
-                dt = 0.02;
+                dt = 0.03;
             }
             Theta0 = Math.PI * Theta0 / 180;
             Alpha0 = Math.PI * Alpha0 / 180;
 
             time = 0;
+            time2 = 0;
             xx = new double[2] { Theta0, Alpha0 };
+            xx2 = new double[2] { Theta0,Alpha0 };
             CompositionTarget.Rendering += StartAnimation;
+            CompositionTarget.Rendering += StartAnimationForGrafics;
         }
         private void StartAnimation(object sender, EventArgs e)
         {
-
             ODESolver.Function[] f = new ODESolver.Function[2] { f1, f2 };
             double[] result = ODESolver.RungeKutta4(f, xx, time, dt);
-
-            double kineticEnergy = 0.5 * PendulumMass * Math.Pow(result[1], 2); // Кинетическая энергия
-            double potentialEnergy = PendulumMass * 9.81 * (PendulumLength - PendulumLength * Math.Cos(result[0])); // Потенциальная энергия
-            double totalEnergy = kineticEnergy + potentialEnergy; // Полная энергия
-            energy.Add(totalEnergy);
-            times.Add(time);
-
-            var energyModel = new PlotModel { };
-
-            var energySeries = new LineSeries
-            {
-                // Установка цвета и толщины линии
-                Color = OxyColors.Blue, // Цвет линии
-                StrokeThickness = 2 // Толщина линии
-            };
-            if (energy.Count > 500) {
-                energy.RemoveAt(0);
-                times.RemoveAt(0);
-            }
-            for (int i = 0;i < energy.Count; i++)
-            {
-                energySeries.Points.Add(new DataPoint(times[i], energy[i]));
-            }
-            energySeries.Points.Add(new DataPoint(time, totalEnergy));
-            energyModel.Series.Add(energySeries);
-            energyPlotView.Model = energyModel;
 
             Point pt = new Point(140 + 130 * Math.Sin(result[0]), 20 + 130 * Math.Cos(result[0]));
             ball.Center = pt;
             line1.X2 = pt.X;
             line1.Y2 = pt.Y;
-
-            if (time < xMax)
-            {
-                pointsX.Add(XNormalize(time) + 10);
-                pointsY.Add(YNormalize(180 * result[0] / Math.PI));
-                if (pointsX.Count > 1000) {
-                    pointsX.RemoveAt(0);
-                    pointsY.RemoveAt(0);
-                }
-
-            }
-            var model = new PlotModel { Title = "Theta - Time Curve" };
-            var series = new LineSeries();
-
-
-
-            for (int i = 0; i < pointsX.Count; i++)
-            {
-                series.Points.Add(new DataPoint(pointsX[i], pointsY[i]));
-            }
-            model.Series.Add(series);
-            plotView.Model = model;
 
             xx = result;
             time += dt;
@@ -159,10 +119,72 @@ namespace Pendulum
                 StopAnim();
             }
         }
+
+        private void  StartAnimationForGrafics(object sender, EventArgs e)
+        {
+            ODESolver.Function[] f = new ODESolver.Function[2] { f1, f2 };
+            double[] result2 = ODESolver.RungeKutta4(f, xx2, time2, dt2);
+            //Расчет энергии
+            double kineticEnergy = 0.5 * PendulumMass * Math.Pow(result2[1], 2); // Кинетическая энергия
+            double potentialEnergy = PendulumMass * 9.81 * (PendulumLength - PendulumLength * Math.Cos(result2[0])); // Потенциальная энергия
+            double totalEnergy = kineticEnergy + potentialEnergy; // Полная энергия
+            energy.Add(totalEnergy);
+            times.Add(time2);
+
+            //Вывод графика энергии
+            var energyModel = new PlotModel { };
+            var energySeries = new LineSeries
+            {
+                // Установка цвета и толщины линии
+                Color = OxyColors.Blue, // Цвет линии
+                StrokeThickness = 2 // Толщина линии
+            };
+            if (energy.Count > 500)
+            {
+                energy.RemoveAt(0);
+                times.RemoveAt(0);
+            }
+            for (int i = 0; i < energy.Count; i++)
+            {
+                energySeries.Points.Add(new DataPoint(times[i], energy[i]));
+            }
+            energySeries.Points.Add(new DataPoint(time2, totalEnergy));
+            energyModel.Series.Add(energySeries);
+            energyPlotView.Model = energyModel;
+            //______________________________________________________________
+            //Вывод графика для координат маятника
+            pointsX.Add(XNormalize(time2) + 10);
+            pointsY.Add(YNormalize(180 * result2[0] / Math.PI));
+            if (pointsX.Count > 1000)
+            {
+                pointsX.RemoveAt(0);
+                pointsY.RemoveAt(0);
+            }
+            
+            var model = new PlotModel { Title = "Theta - Time Curve" };
+            var series = new LineSeries();
+
+            for (int i = 0; i < pointsX.Count; i++)
+            {
+                series.Points.Add(new DataPoint(pointsX[i], pointsY[i]));
+            }
+            model.Series.Add(series);
+            plotView.Model = model;
+
+            xx2 = result2;
+            time2 += dt2;
+            if (time > 0 && Math.Abs(result2[0]) < 0.01 && Math.Abs(result2[1]) < 0.001)
+            {
+                StopAnim();
+            }
+
+        }
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
             pointsX.Clear();
             pointsY.Clear();
+            energy.Clear();
+            times.Clear();
 
             var model = new PlotModel { Title = "Theta - Time Curve" };
 
@@ -170,15 +192,26 @@ namespace Pendulum
             model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, Minimum = yMin, Maximum = yMax });
 
             plotView.Model = model;
+            var energyModel = new PlotModel { };
+
+            var energySeries = new LineSeries
+            {
+                // Установка цвета и толщины линии
+                Color = OxyColors.Blue, // Цвет линии
+                StrokeThickness = 2 // Толщина линии
+            };
+            energyModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, Minimum = xMin, Maximum = xMax });
+            energyModel.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, Minimum = yMin, Maximum = yMax });
+            energyPlotView.Model = energyModel;
             PendulumInitialize();
             StopAnim();
         }
 
-        private void energyOxy() { }
         private void StopAnim()
         {
 
             CompositionTarget.Rendering -= StartAnimation;
+            CompositionTarget.Rendering -= StartAnimationForGrafics;
         }
 
         private void PendulumInitialize()
